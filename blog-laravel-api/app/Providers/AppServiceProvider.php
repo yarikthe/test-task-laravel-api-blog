dfe823b6f66010ffe1567d9702d809181bf492fc
Yaroslav Lukyanchuk <yarikthe@gmail.com>
2021-07-04 02:45:19 +0300

    migrate basic

--- blog-laravel-api/app/Providers/AppServiceProvider.php
+++ blog-laravel-api/app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +26,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultstringLength(191);
     }
 }
